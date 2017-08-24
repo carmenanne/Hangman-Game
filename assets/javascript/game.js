7141f7519f45ed9537e82e60c4aa4ed87420f2fe
Carmen Stockberger <carmen.stockberger@gmail.com>
2017-08-24 00:26:54 -0700

    still working on the for and if/else code, WIP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,89 @@
-//page opens
-var movie = [['t', 'i', 't', 'a', 'n', 'i', 'c'], ['a', ' ', 'f', 'e', 'w', ' ', 'g', 'o', 'o', 'd', ' ', 'm', 'e', 'n'], 
-['f', 'o', 'r', 'r', 'e', 's', 't', ' ', 'g', 'u', 'm', 'p'], ['h', 'o', 'm', 'e', ' ', 'a', 'l', 'o', 'n', 'e']];
-var random = Math.floor((Math.random()*(movie.length)));
-var movie = movie[random];
+//movie titles 
+var movieList = [
+	['t','i','t','a','n','i','c'],
+	['j','a','w','s'],
+	['i','n','c','e','p','t','i','o','n']
 
-var movieChoice = new
-Array(movie.length);
-var start = 0;
-var wins = 0;
-console.log(movie)
+	// "a few good men",
+	// "home alone",
+	// "titanic",
+	// "forrest gump",
+	// "fight club",
+	// "the big lebowski",
+]
+var random = Math.floor((Math.random() * (movieList.length-1)));
+var movieChoice = movieList[random];
+var movieSelection = new Array(movieChoice.length);
+var error = 0
+//var lettersGuessed = event.key;
 
+//# of guesses remaining
 var guessesLeft = 12
-//press any key to start the game
-document.onkeyup = function(event) {
-console.log(movieChoice)
-document.getElementById('start').innerHTML = ""
-document.getElementById('guessesLeft').innerHTML = guessesLeft
-}
-//game starts
-	
-//computer selects a word from list at random
-document.onkeyup = function(event) {
-console.log(movieChoice)
-document.getElementById('start').innerHTML = ""
+//letters already guessed
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//# of wins
+var wins = 0
+//# of losses
+var losses = 0
 
-//generates the word as "_" for each letter
- 	for (var i = 0; i < movieChoice.length; i ++){
- 	movieChoice[i] = "_ "
- 	var lettersGuessed = event.key;
-		document.getElementById('currentWord').innerHTML = movieChoice;
-		
+for (var i = 0; i < movieSelection.length; i ++){
+ 		   		movieSelection[i] = "_ ";
+ 		 	console.log(movieChoice)
+ 		 	;
+ 		 };
+
+//any key starts the game
+document.onkeyup = function(){
+	document.getElementById("start").innerHTML = "";
+	document.getElementById("guessesLeft").innerHTML = guessesLeft --;
+	document.getElementById("wins").innerHTML = "Wins: " + wins;
+	document.getElementById("losses").innerHTML = "Losses: " + losses;
+	
+	
+
+
+		document.getElementById("currentWord").innerText = movieSelection
+	
+	function print(){
+		for (var i = 0; i < movieSelection.length; i++){
+			var currentWord =
+			document.getElementById("currentWord");
+			var correctLetters = 
+			document.createTextNode(movieSelection[i]);
+			currentWord.appendChild(correctLetters);
+		}
 	}
-
-//if letter matches letter in random movie title...
-	// if (lettersGuessed.forEach(movieChoice(element))
-	// //letter(s) appears on screen
-	// 	document.getElementById("_").innerHTML = movieChoice(element);
+	console.log(movieSelection);
 	
-	
-	//number of guesses goes down by 1
-	
+var lettersGuessed = event.key;
+ if (lettersGuessed===letters.length){
 
-//if letter doesn't match letter in random movie title...
-	//display letter in "letters guessed"
-	//number of guesses goes down by 1
+	 if (lettersGuessed===movieSelection[i]) function(){
+	 	var currentWord = document.getElementById("currentWord");
+	 		currentWord.innerText="";
+	 		printcurrentWord()
+	}
+};
+	// letters.forEach(function(guess){
+	// var lettersGuessed = event.key
+	// document.getElementById("lettersGuessed");
+	// var guesses = 
+	// document.createTextNode(letters[i]);
+	//lettersGuessed.appendChild(guesses);
+		 	
+		document.getElementById("lettersGuessed").innerText = "Letters guessed: " + guesses;
+		console.log(lettersGuessed)
+	}
+ 		
 
-//if movie title is guessed, wins increases by 1
-	//restart game
-//if player runs out of guesses, display losing banner
-	//restart game
-document.getElementById('guessesLeft').innerHTML = guessesLeft - 1;
-}
+//if player guesses letter not in title, it appears (append) on letters guessed
+
+//if player guesses all letters in a title, win + 1
+
+//if # of guesses remaining = 0, lose +1
+
+ };
+
+
+
+
